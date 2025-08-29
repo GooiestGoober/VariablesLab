1a3a5d2d92fa60e3d68a9c670b1859b33dd4a5da
GooiestGoober <octavio.cervera@allsaintshigh.org>
2025-08-29 10:58:55 -0500

    asdf111

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,48 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        //define 1 variable of each of the
+//following data types
+//byte short int long
+//float double
+//char boolean String
+//integer variables
+        byte byteOne = 113;
+        short shortOne = 32222;
+        int intOne = 2111111111;
+        long longOne = 922000000000l;
+        float  floatOne = 4100000000f;
+        double doubleOne = 1200000000000000000l;
+        char charOne = '0';
+        boolean booleanOne = false;
+        String stringOne = "doodoofard";
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println("/////////////////////////////////");
+        System.out.println("*Octavio Cervera        08/29/25*");
+        System.out.println("*                               *");
+        System.out.println("*         integer types         *");
+        System.out.println("*                               *");
+        System.out.println("*8 bit - byteOne = "+byteOne+"\t\t    *");
+        System.out.println("*16 bit - shortOne = "+shortOne+"      *");
+        System.out.println("*32 bit - intOne = "+intOne+"   *");
+        System.out.println("*64 bit - longOne = "+longOne+"*");
+        System.out.println("*                               *");
+        System.out.println("*         real types            *");
+        System.out.println("*                               *");
+        System.out.println("*32 bit - floatOne = "+floatOne+"\t\t*");
+        System.out.println("*64 bit - doubleOne = "+doubleOne+"    *");
+        System.out.println("*                               *");
+        System.out.println("*         other integer types   *");
+        System.out.println("*                               *");
+        System.out.println("*16 bit - charOne = "+charOne+"\t\t    *");
+        System.out.println("*                               *");
+        System.out.println("*         other types           *");
+        System.out.println("*                               *");
+        System.out.println("*1 bit - booleanOne = "+booleanOne+"\t\t*");
+        System.out.println("*16 bit - stringOne = "+stringOne+"*");
+        System.out.println("/////////////////////////////////");
+
+
+
     }
 }
